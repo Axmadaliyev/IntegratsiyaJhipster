8a90da429d4657001bb1891074656b720d3065f9
user <axmadaliyevfarruxbek207@gmail.com>
2022-09-20 22:39:52 +0500

    Initial version of IntegratsiyaJhipster generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/myproject/app/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.myproject.app.web.rest;
---
+++ src/test/java/com/myproject/app/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.myproject.app;
+
+import com.myproject.app.IntegratsiyaJhipsterApp;
+import com.myproject.app.config.AsyncSyncConfiguration;
+import com.myproject.app.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { IntegratsiyaJhipsterApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
